fcd5594f7a8a85ab6854309841180fc3adfd2209
Narin-Ball <70697092+Narin-sang1991@users.noreply.github.com>
2025-09-06 09:34:49 +0700

    Device Control

---
+++ AppConfigurationModel.cs
@@ -0,0 +1,14 @@
+﻿using Ints.Core.Model.Enums;
+
+namespace Ints.Core.Model
+{
+    public record AppConfigurationRequestModel
+    {
+        public Guid? Id { get; init; }
+        public string Code { get; init; }
+        public string? Name { get; init; }
+        public string? Description { get; init; }
+        public ConfigType ValueTypeEnum { get; init; }
+        public string ConfigurationValue { get; init; }
+    }
+}
---
+++ Enums/ConfigType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ints.Core.Model.Enums
+{
+    public enum ConfigType
+    {
+        InputString = 1,
+        InputNumber = 2,
+        RadioButton = 3,
+        Checkbox = 4,
+        Selector = 5,
+        ImageURL = 6,
+        RichText = 7,
+        Html = 8,
+    }
+}
