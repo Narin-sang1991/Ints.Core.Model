c447499ac1d2aefdb0b3adb90392d66b40988a1d
Narin-Ball <70697092+Narin-sang1991@users.noreply.github.com>
2025-09-15 02:29:47 +0700

    Create User Service

---
+++ UserCriteriaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ints.Core.Model
+{
+    public record UserCriteria
+    {
+        public Guid? UserId { get; set; }
+        public string? Login { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
--- UserSaveDataModel.cs
+++ UserSaveDataModel.cs
@@ -1,16 +1,19 @@
 ﻿using Ints.Core.Model.Enums;
+using System.Text.Json.Serialization;
 
 namespace Ints.Core.Model
 {
     public record UserSaveDataModel
     {
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public bool IsActive { get; set; }
-        public AuthType? AuthType { get; set; }
         public bool IsRequiredResetPassword { get; set; }
+
+        [JsonIgnore]
+        public AuthType? AuthType { get; set; }
     }
 }
