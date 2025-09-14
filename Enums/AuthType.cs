a0949173d53aaf7de600e5492d486816a01a2a4c
Narin-Ball <70697092+Narin-sang1991@users.noreply.github.com>
2025-09-15 01:14:59 +0700

    AppConfiguration & User

---
+++ Enums/AuthType.cs
@@ -0,0 +1,10 @@
+﻿namespace Ints.Core.Model.Enums
+{
+    public enum AuthType
+    {
+        InternalAccount = 110,
+        ExternalAccount = 120,
+        OtpAccount = 130,
+        Ldap = 140,
+    };
+}
---
+++ UserSaveDataModel.cs
@@ -0,0 +1,16 @@
+﻿using Ints.Core.Model.Enums;
+
+namespace Ints.Core.Model
+{
+    public record UserSaveDataModel
+    {
+        public Guid UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public AuthType? AuthType { get; set; }
+        public bool IsRequiredResetPassword { get; set; }
+    }
+}
